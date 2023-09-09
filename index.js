e09518af541d079aa719f32de22c750eb8c8a30f
Gaurav <gauravsaraiwala20@gmail.com>
2023-09-09 22:35:42 +0530

    New

--- index.js
+++ index.js
@@ -3,11 +3,13 @@ const express = require("express");
 const notes = require('./routes/notes')
 const auth = require('./routes/auth')
 const app = express();
+const cors = require('cors')
 const dotenv = require('dotenv')
 dotenv.config()
 
 connectToMongo();
 
+app.use(cors)
 app.use(express.json())
 //Available Routes
 app.use('/auth', auth)
